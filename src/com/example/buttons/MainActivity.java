f92d93b647bade56db7b6efd79c2246eb2b02f7a
JohnnyPP <johnnnyppp@gmail.com>
2013-08-28 22:58:29 +0200

    Edit view sends data to text view

--- src/com/example/buttons/MainActivity.java
+++ src/com/example/buttons/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -24,7 +25,15 @@ public class MainActivity extends Activity {
     
     public void buttonClick(View v)
     {
+    	CharSequence temp;
     	TextView textView = (TextView)findViewById(R.id.textView); //textView is the ID added to the Hello World! string
-    	textView.setText("Test");
+    	TextView tw = (TextView)findViewById(R.id.textView1);
+    	EditText editText = (EditText) findViewById(R.id.editText1);
+    	
+    	String message = editText.getText().toString();
+    	
+    	//temp=tw.getText();
+    	tw.setText(message + "94584");
+    	textView.setText(message);
     }
 }
